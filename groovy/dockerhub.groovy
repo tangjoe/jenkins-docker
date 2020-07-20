aaf36dc7c5e730db73e6ea31c9eb89c2bfbc5ec7
Joe Tang <tangwcjoe@gmail.com>
2020-07-20 17:50:49 +0800

    Make changes

--- groovy/dockerhub.groovy
+++ groovy/dockerhub.groovy
@@ -14,8 +14,8 @@ usernameAndPassword = new UsernamePasswordCredentialsImpl(
   CredentialsScope.GLOBAL,
   "dockerhub",
   "Dockerhub login",
-  "tangjoe88",
-  "passw0rd"
+  "USERNAME",
+  "PASWORD"
 )
 
 store.addCredentials(domain, usernameAndPassword)
